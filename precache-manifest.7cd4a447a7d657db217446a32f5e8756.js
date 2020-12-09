71381d7962a63f47a3912ecf07e043912cb94a92
lokicoule <lokicoule@yahoo.fr>
2020-12-09 23:59:15 +0100

    Updates

---
+++ precache-manifest.7cd4a447a7d657db217446a32f5e8756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e4a8bfead9882166c9db6a5640fc8d",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/index.html"
+  },
+  {
+    "revision": "9c63ff2ab09467ff07b2",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/static/css/main.2851464b.chunk.css"
+  },
+  {
+    "revision": "e27a18e0d01e4eef1077",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/static/js/2.5343062f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/static/js/2.5343062f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c63ff2ab09467ff07b2",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/static/js/main.3ab37cc0.chunk.js"
+  },
+  {
+    "revision": "4d183b92c0e85eaf4c08",
+    "url": "/Lokicoule/sorting-viewer-and-comparator/static/js/runtime-main.381ff65b.js"
+  }
+]);
